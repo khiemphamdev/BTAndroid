2aef67cc091d6b987b1443743f28fb51d09b89fa
khiemphamdev <khiempham.dev@gmail.com>
2025-11-12 17:03:33 +0700

    First commit

---
+++ FragmentEx_Replace/app/src/main/java/clc65/phamgiakhiem/fragmentex_replace/FooterFragment.java
@@ -0,0 +1,61 @@
+package clc65.phamgiakhiem.fragmentex_replace;
+
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.button.MaterialButton;
+
+public class FooterFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_footer, container, false);
+        //find view
+        MaterialButton b1 = v.findViewById(R.id.button1);
+        MaterialButton b2 = v.findViewById(R.id.button2);
+        MaterialButton b3 = v.findViewById(R.id.button3);
+        //
+        FragmentManager fragmentManager = getParentFragmentManager();
+        //event
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new OneFragment())
+                        .commit();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new TwoFragment())
+                        .commit();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new ThreeFragment())
+                        .commit();
+            }
+        });
+        return v;
+    }
+}
---
+++ FragmentEx_Replace/app/src/main/java/clc65/phamgiakhiem/fragmentex_replace/OneFragment.java
@@ -0,0 +1,27 @@
+package clc65.phamgiakhiem.fragmentex_replace;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OneFragment extends Fragment {
+    public OneFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
